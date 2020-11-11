88ece6e57670e426fe7fc1dae4cd01e2474e9c76
Admin <kostian.mendes@gmail.com>
2020-11-11 16:13:09 +0200

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import UserList from "./UserList";
 import React, {Component, useState} from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
 import usernames from '../fictures'
 import {Row, Col} from "antd";
 import ChangeName from "./changeName";
 import 'antd/dist/antd.css';
 
 function App() {
-    const listr = useState( true)
-    const handleChange = () => {
-        console.log('спрятали елемент')
-    }
-
     return (
         <div style={{marginTop: '140px'}}>
             <Row>
@@ -33,11 +28,6 @@ function App() {
             </Row>
         </div>
     )
-    function HandleClick() {
-        useState(null)
-        alert('User is deleted22!')
-    }
 }
-
 
 export default App
